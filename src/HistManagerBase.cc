6c8f795b58d1cb9dc6f1bebf803b85d6e95ad2bb
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2009-02-04 15:56:39 +0000

    moved code into Core package

--- src/HistManagerBase.cc
+++
@@ -1,6 +0,0 @@
-#include "TauAnalysis/RecoTools/interface/HistManagerBase.h"
-
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-EDM_REGISTER_PLUGINFACTORY(HistManagerPluginFactory, "HistManagerPluginFactory");
-
