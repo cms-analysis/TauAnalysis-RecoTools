7282dc8c92450903564ba32a7536fe00f1c957f7
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-01-07 13:00:47 +0000

    added support for handling of systematic uncertainties

--- python/eventSelFlagProdConfigurator.py
+++ python/eventSelFlagProdConfigurator.py
@@ -2,6 +2,8 @@ import FWCore.ParameterSet.Config as cms
 import copy
 import sys
 
+from TauAnalysis.CandidateTools.tools.objSelConfigurator import *
+
 #--------------------------------------------------------------------------------
 # utility function for configuring BoolEventSelFlagProducer modules
 #
@@ -16,7 +18,7 @@ class eventSelFlagProdConfigurator(cms._ParameterTypeBase):
         self.boolEventSelFlagProducer = boolEventSelFlagProducer
         self.pyModuleName = pyModuleName
 
-    def _addModule(self, objSelItem, srcParam):
+    def _addModule(self, objSelItem, srcParam, sysName = None):
         # create module
         module = cms.EDFilter(self.boolEventSelFlagProducer)
 
@@ -26,7 +28,7 @@ class eventSelFlagProdConfigurator(cms._ParameterTypeBase):
             # multiple plugin case;
             # prepare configuration parameters for plugins
             # derrived from EventSelectorBase class
-            exclAttributeNames = ["src_cumulative", "src_individual"]
+            exclAttributeNames = [ "src_cumulative", "src_individual" ]
             selector = cms.PSet()
             for objSelAttrName in dir(objSelItem):
                 objSelAttr = getattr(objSelItem, objSelAttrName)
@@ -37,7 +39,10 @@ class eventSelFlagProdConfigurator(cms._ParameterTypeBase):
             for src_i in srcParam:
                 assert len(src_i) == 2, "'src_i' Parameter must have exactly 2 elements !!"
                 selector_i = copy.deepcopy(selector)
-                setattr(selector_i, "src", src_i[0])
+                if sysName is None:
+                    setattr(selector_i, "src", src_i[0])
+                else:
+                    setattr(selector_i, "src", objSelConfigurator._composeModuleName(src_i[0], sysName))
                 if len(src_i) >= 2 : setattr(selector_i, "instanceName", cms.string(src_i[1]))
                 selectors.append(selector_i)
 
@@ -45,13 +50,25 @@ class eventSelFlagProdConfigurator(cms._ParameterTypeBase):
         else:
             # single plugin case;
             # add configuration parameters directly to module
+            exclAttributeNames = [ "src", ]
             for objSelAttrName in dir(objSelItem):
                 objSelAttr = getattr(objSelItem, objSelAttrName)
-                if isinstance(objSelAttr, cms._ParameterTypeBase):
+                if isinstance(objSelAttr, cms._ParameterTypeBase) and not objSelAttrName in exclAttributeNames:
                     setattr(module, objSelAttrName, objSelAttr)
 
+            src = srcParam[0]
+            if sysName is None:
+                setattr(module, "src", src[0])
+            else:
+                setattr(module, "src", cms.InputTag(objSelConfigurator._composeModuleName(src[0].value(), sysName)))
+            if len(src) >= 2 : setattr(module, "instanceName", cms.string(src[1]))
+
         # set module attributes
-        moduleName = objSelItem.pluginName.value()
+        moduleName = None
+        if sysName is None:
+            moduleName = objSelItem.pluginName.value()
+        else:
+            moduleName = objSelConfigurator._composeModuleName(objSelItem.pluginName.value(), sysName)
         module.setLabel(moduleName)
                
         # register module in global python name-space
@@ -59,8 +76,6 @@ class eventSelFlagProdConfigurator(cms._ParameterTypeBase):
         if pyModule is None:
             raise ValueError("'pyModuleName' Parameter invalid !!")
         setattr(pyModule, moduleName, module)
-        
-        self.lastModuleName = moduleName
 
         # add module to sequence
         if self.sequence == None:
@@ -84,6 +99,7 @@ class eventSelFlagProdConfigurator(cms._ParameterTypeBase):
             src = None
             src_cumulative = None
             src_individual = None
+            systematics = None
             for objSelAttrName in dir(objSelItem):
                 if objSelAttrName == "src":
                     src = getattr(objSelItem, objSelAttrName)
@@ -91,14 +107,22 @@ class eventSelFlagProdConfigurator(cms._ParameterTypeBase):
                     src_cumulative = getattr(objSelItem, objSelAttrName)
                 if objSelAttrName == "src_individual":
                     src_individual = getattr(objSelItem, objSelAttrName)
+                if objSelAttrName == "systematics":
+                    systematics = getattr(objSelItem, objSelAttrName)
 
             if ( src is not None and src_cumulative is not None and src_individual is not None ) or \
                ( src is None and (src_cumulative is None or src_individual is None) ):
                 raise ValueError("must specify either 'src' or 'src_cumulative' and 'src_individual' Parameters !!")
 
             if src is not None:
-                self._addModule(objSelItem, [ src, ] )
+                self._addModule(objSelItem, [ [ src, ] ] )
+                if systematics is not None:
+                    for sysName in systematics:
+                        self._addModule(objSelItem, [ src, ], sysName )
             if src_cumulative is not None and src_individual is not None:
                 self._addModule(objSelItem, [ [ src_cumulative, "cumulative" ], [ src_individual, "individual" ] ])
+                if systematics is not None:
+                    for sysName in systematics:
+                        self._addModule(objSelItem, [ [ src_cumulative, "cumulative" ] ], sysName)
 
         return cms.Sequence(self.sequence)
