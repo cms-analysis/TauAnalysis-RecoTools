5552b721fa55c95d3ef812b174f9326b74d6a8d8
Michail Bachtis <sha1-eaf6ea55826e5289c86058cfc492a286c076afdf@cern.ch>
2010-01-27 11:30:55 +0000

    Updating systematics

--- plugins/SmearedParticleProducer.h
+++ plugins/SmearedParticleProducer.h
@@ -1,10 +1,17 @@
+#ifndef TauAnalysis_SmearedParticleProducer
+#define TauAnalysis_SmearedParticleProducer
+/* Smeared Particle Producer 
+Module that changes the energy scale PT, ETA 
+for a general PAT Particle 
+Author : Michail Bachtis 
+University of Wisconsin
+*/
 // system include files
 #include <memory>
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDProducer.h"
-
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Utilities/interface/Exception.h"
 
@@ -20,7 +27,7 @@
 #include "CLHEP/Random/RandGauss.h"
 #include <TFile.h>
 #include <TH1F.h>
-
+/*Helper classes to return the generated Lorentz Vector in case of leptons and jets*/
 
 template <typename T>
 class GenParticleRetriever {
@@ -28,7 +35,7 @@ class GenParticleRetriever {
   GenParticleRetriever() {}
   ~GenParticleRetriever() {}
   GenParticleRetriever(const T& particle) {
-    if(particle.genParticle()>0)
+    if(particle.genParticle()!=0)
       p4_ = particle.genParticle()->p4();
     else
       p4_ = particle.p4();
@@ -47,48 +54,45 @@ class GenJetRetriever {
   GenJetRetriever() {}
   ~GenJetRetriever() {}
   GenJetRetriever(const T& particle) {
-    if(particle.genJet()>0)
+    if(particle.genJet()!=0)
       p4_ = particle.genJet()->p4();
     else
       p4_ = particle.p4();
   }
-
   math::XYZTLorentzVector genP4() { return p4_; }
-  
  private:
   math::XYZTLorentzVector p4_;
-
 };
 
+
+/*end helper classes*/
 
 
 template <typename T,typename G>
 class SmearedParticleProducer : public edm::EDProducer {
    public:
   explicit SmearedParticleProducer(const edm::ParameterSet& iConfig):
-      src_(iConfig.getParameter<edm::InputTag>("src")),  
-      fileName_(iConfig.getParameter<edm::FileInPath>("fileName")),  
-      smearMCParticle_(iConfig.getParameter<bool>("smearMCParticle")),  
-      smearFromPtHistogram_(iConfig.getParameter<bool>("smearFromPtHistogram")),  
-      smearFromEtaHistogram_(iConfig.getParameter<bool>("smearFromEtaHistogram")),  
-      ptHisto_(iConfig.getParameter<std::string>("ptHistogram")),			  
-      etaHisto_(iConfig.getParameter<std::string>("etaHistogram")),			  
-      energyScale_(iConfig.getParameter<double>("energyScale")),
-      deltaEta_(iConfig.getParameter<double>("deltaEta")),
-      deltaPt_(iConfig.getParameter<double>("deltaPt")),
-      gSigmaPt_(iConfig.getParameter<double>("gaussianSmearingSigmaPt")),
-      gSigmaEta_(iConfig.getParameter<double>("gaussianSmearingSigmaEta")) 
+    src_(iConfig.getParameter<edm::InputTag>("src")),  
+    fileName_(iConfig.getParameter<edm::FileInPath>("fileName")),  
+    smearMCParticle_(iConfig.getParameter<bool>("smearMCParticle")),  
+    smearFromPtHistogram_(iConfig.getParameter<bool>("smearFromPtHistogram")),  
+    smearFromEtaHistogram_(iConfig.getParameter<bool>("smearFromEtaHistogram")),  
+    ptHisto_(iConfig.getParameter<std::string>("ptHistogram")),			  
+    etaHisto_(iConfig.getParameter<std::string>("etaHistogram")),			  
+    energyScale_(iConfig.getParameter<double>("energyScale")),
+    deltaEta_(iConfig.getParameter<double>("deltaEta")),
+    deltaPt_(iConfig.getParameter<double>("deltaPt")),
+    gSigmaPt_(iConfig.getParameter<double>("gaussianSmearingSigmaPt")),
+    gSigmaEta_(iConfig.getParameter<double>("gaussianSmearingSigmaEta")) 
     {
-      //Check if the file is there
+      //Check if the file with the histograms is there
       if(smearFromPtHistogram_||smearFromEtaHistogram_) {
 	if(fileName_.isLocal()) {
 	  f = new TFile(fileName_.fullPath().c_str());
-	    
 	  if(smearFromPtHistogram_) {
 	    TH1F *tmp = (TH1F*)f->Get(ptHisto_.c_str());
 	    ptHisto = integratedHisto(tmp);
 	  }
-	  
 	  if(smearFromEtaHistogram_) {
 	    TH1F *tmp = (TH1F*)f->Get(etaHisto_.c_str());
 	    etaHisto = integratedHisto(tmp);
@@ -100,180 +104,170 @@ class SmearedParticleProducer : public edm::EDProducer {
 	
       }
 
+      //Initalize random number service
+	if ( ! rng.isAvailable()) {
+	  throw cms::Exception("Configuration")
+	    << "DistortedTauProducer requires the RandomNumberGeneratorService\n"
+	    "which is not present in the configuration file.  You must add the service\n"
+	    "in the configuration file or remove the modules that require it.";
+	}
+
+      CLHEP::HepRandomEngine& randomEngine = rng->getEngine();
+      flatDistribution = new CLHEP::RandFlat(randomEngine, 0., 1.);
+      gaussPt = new CLHEP::RandGauss(randomEngine, 0.,gSigmaPt_ );
+      gaussEta = new CLHEP::RandGauss(randomEngine, 0.,gSigmaEta_ );
+
       produces<std::vector<T> >();
     }
 
+  ~SmearedParticleProducer() {
+    if(smearFromPtHistogram_||smearFromEtaHistogram_) 
+	f->Close();
 
-
-  ~SmearedParticleProducer() {}
+    if(ptHisto !=0) 
+      delete ptHisto;
+    if(etaHisto !=0) 
+      delete etaHisto;
+    if(flatDistribution !=0)
+      delete flatDistribution;
+    if(gaussPt !=0)
+      delete gaussPt;
+    if(etaHisto !=0)
+      delete gaussEta;
+  }
     
    protected:
-  virtual void beginJob(const edm::EventSetup& es) {}
-  virtual void endJob() {}
+     void smear(T& object)
+       {
 
-  void smear(T& object)
-    {
-            
-	math::XYZTLorentzVector vToSmear;
 
+	 math::XYZTLorentzVector vToSmear;
+	 
+	 //Decide which LV to smear
+	 if(smearMCParticle_){
+	   //check if it matched to a gen jet or a gen particle
+	   G mcRetriever(object);
+	   vToSmear = mcRetriever.genP4(); //If it is not matched it will return the reco p4
+	 }
+	 else {
+	   vToSmear = object.p4();
+	 }
       
-	//Decide which LV to smear
-	if(smearMCParticle_){
-	  //check if it matched to a gen jet or a gen particle
-
-	  G mcRetriever(object);
-	  vToSmear = mcRetriever.genP4(); //If it is not matched it will return the reco p4
-	}
-	else {
-	  vToSmear = object.p4();
-	}
-      
-	//apply energy scale!
-	math::PtEtaPhiMLorentzVector vecES(vToSmear.pt()*energyScale_,vToSmear.eta(),vToSmear.phi(),vToSmear.M());
-	math::XYZTLorentzVector cartVecES(vecES.px(),vecES.py(),vecES.pz(),vecES.energy());
-	object.setP4(cartVecES);
+	 //apply energy scale!
+	 //following Christian's suggestion and applying energy scale in the whole Lorentz Vector
+	 math::XYZTLorentzVector cartVecES = energyScale_*vToSmear;
+	 object.setP4(cartVecES);
+	 
+	 //apply pt and eta DISPLACEMENTS
+	 math::PtEtaPhiMLorentzVector vec(object.pt()+deltaPt_,object.eta()+deltaEta_,object.phi(),object.mass());
+	 math::XYZTLorentzVector cartVec(vec.px(),vec.py(),vec.pz(),vec.energy());
+	 object.setP4(cartVec);
+	 
+	 //Apply gaussian smearing
+	 double deltaPt = gaussPt->fire();
+	 double deltaEta = gaussEta->fire();
+	 math::PtEtaPhiMLorentzVector vecGauss(object.pt()+deltaPt,object.eta()+deltaEta,object.phi(),object.mass());
+	 math::XYZTLorentzVector cartVecGauss(vecGauss.px(),vecGauss.py(),vecGauss.pz(),vecGauss.energy());
+	 object.setP4(cartVecGauss);
+	
+	 //OK Now histogram Smearing
+	 double hDeltaPt = 0.0;
+	 double hDeltaEta = 0.0;
+	 
+	 if(smearFromPtHistogram_) {
+	   double randomNumber = flatDistribution->fire();
+	   hDeltaPt = sampleFromHisto(ptHisto,randomNumber);
+	 }
+	 if(smearFromEtaHistogram_) {
+	   double randomNumber2 = flatDistribution->fire();
+	  hDeltaEta = sampleFromHisto(etaHisto,randomNumber2);
+	 }
+	 math::PtEtaPhiMLorentzVector vecH(object.pt()+hDeltaPt,object.eta()+hDeltaEta,object.phi(),object.mass());
+	 math::XYZTLorentzVector cartVecH(vecH.px(),vecH.py(),vecH.pz(),vecH.energy());
+	 object.setP4(cartVecH);
+       }
      
-      //apply pt and eta
-      //In eta since we need it for acceptabnce reasons use a sign convension
-      
-      double deta=0;
-      if(object.eta()>0)
-	deta = deltaEta_;
-      else
-	deta = -deltaEta_;
-      math::PtEtaPhiMLorentzVector vec(object.pt()+deltaPt_,object.eta()+deta,object.phi(),object.mass());
-      math::XYZTLorentzVector cartVec(vec.px(),vec.py(),vec.pz(),vec.energy());
-      object.setP4(cartVec);
-      
-      //Apply gaussian smearing
-      if ( ! rng.isAvailable()) {
-	throw cms::Exception("Configuration")
-	  << "DistortedTauProducer requires the RandomNumberGeneratorService\n"
-		  "which is not present in the configuration file.  You must add the service\n"
-	  "in the configuration file or remove the modules that require it.";
-      }
-      
-      CLHEP::HepRandomEngine& engine = rng->getEngine();
-      
-      CLHEP::RandFlat* flatDistribution = new CLHEP::RandFlat(engine, 0., 1.);
-      CLHEP::RandGauss* gaussPt = new CLHEP::RandGauss(engine, 0.,gSigmaPt_ );
-      CLHEP::RandGauss* gaussEta = new CLHEP::RandGauss(engine, 0.,gSigmaEta_ );
-      
-      double deltaPt = gaussPt->fire();
-      double deltaEta = gaussEta->fire();
-      math::PtEtaPhiMLorentzVector vecGauss(object.pt()+deltaPt,object.eta()+deltaEta,object.phi(),object.mass());
-      math::XYZTLorentzVector cartVecGauss(vecGauss.px(),vecGauss.py(),vecGauss.pz(),vecGauss.energy());
-      object.setP4(cartVecGauss);
-      
-      
-      //OK Now histogram Smearing
-      double hDeltaPt = 0.0;
-      double hDeltaEta = 0.0;
-      
-      if(smearFromPtHistogram_) {
-	double randomNumber = flatDistribution->fire();
-	hDeltaPt = sampleFromHisto(ptHisto,randomNumber);
-      }
-      
-      if(smearFromEtaHistogram_) {
-	double randomNumber2 = flatDistribution->fire();
-	hDeltaEta = sampleFromHisto(etaHisto,randomNumber2);
-      }
-      
-      math::PtEtaPhiMLorentzVector vecH(object.pt()+hDeltaPt,object.eta()+hDeltaEta,object.phi(),object.mass());
-      math::XYZTLorentzVector cartVecH(vecH.px(),vecH.py(),vecH.pz(),vecH.energy());
-      object.setP4(cartVecH);
-      
-      delete flatDistribution;
-      delete gaussPt;
-      delete gaussEta;
-    }
-
-  virtual void produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
-    using namespace edm;
-
-    std::auto_ptr<std::vector<T> > out(new std::vector<T> );
-    Handle<std::vector<T> > srcH;
-  
-    if(iEvent.getByLabel(src_,srcH) &&srcH->size()>0) 
-      for(unsigned int i=0;i<srcH->size();++i) {
-	T  object = srcH->at(i);
-	smear(object);
-	out->push_back(object);
-      }
-
-    iEvent.put(out);
-
-  } 
-
-  TH1F* integratedHisto(TH1F* histo) {
-    histo->Scale(1/histo->Integral());
-    TH1F * intHisto = new TH1F("intHisto","",histo->GetNbinsX(),histo->GetXaxis()->GetXmin(),histo->GetXaxis()->GetXmax());
-    float integral = 0.0;
-    for(int i=1;i<=histo->GetNbinsX();++i) {
-      integral+=histo->GetBinContent(i);
-      intHisto->SetBinContent(i,integral);
-    }
-    return intHisto;
-  }
+     virtual void produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
+       using namespace edm;
+       
+       std::auto_ptr<std::vector<T> > out(new std::vector<T> );
+       Handle<std::vector<T> > srcH;
+       
+       if(iEvent.getByLabel(src_,srcH)) 
+	 for(unsigned int i=0;i<srcH->size();++i) {
+	   T  object = srcH->at(i);
+	   smear(object);
+	   out->push_back(object);
+	 }
+       
+       iEvent.put(out);
+       
+     } 
 
 
-  double sampleFromHisto(TH1F * histo, double x) {
-    double min = histo->GetMinimum();
-    double max = histo->GetMaximum();
-    int  bins  = histo->GetNbinsX();
-    int bin=0;
-    for(int i=1;i<=bins;++i) {
-      
-      double low;
-    double high;
-    
-    if(i==1) {
-      low=min;
-      high =  histo->GetBinContent(2);
-    }
-    else if (i==bins) {
-      low= histo->GetBinContent(bins-1);
-      high= max;
-    }
-    else {
-      low  =  histo->GetBinContent(i-1);
-      high =  histo->GetBinContent(i+1);
-    }
-    
-    if(x>low&&x<=high)
-      bin=i;
-    
-    }
+     TH1F* integratedHisto(TH1F* histo) {
+       histo->Scale(1/histo->Integral());
+       TH1F * intHisto = new TH1F("intHisto","",histo->GetNbinsX(),histo->GetXaxis()->GetXmin(),histo->GetXaxis()->GetXmax());
+       float integral = 0.0;
+       for(int i=1;i<=histo->GetNbinsX();++i) {
+	 integral+=histo->GetBinContent(i);
+	 intHisto->SetBinContent(i,integral);
+       }
+       return intHisto;
+     }
+
+
+     double sampleFromHisto(TH1F * histo, double x) {
+       double min = histo->GetMinimum();
+       double max = histo->GetMaximum();
+       int  bins  = histo->GetNbinsX();
+       int bin=0;
+       for(int i=1;i<=bins;++i) {
+	 
+	 double low;
+	 double high;
+	 
+	 if(i==1) {
+	   low=min;
+	   high =  histo->GetBinContent(2);
+	 }
+	 else if (i==bins) {
+	   low= histo->GetBinContent(bins-1);
+	   high= max;
+	 }
+	 else {
+	   low  =  histo->GetBinContent(i-1);
+	   high =  histo->GetBinContent(i+1);
+	 }
+	 
+	 if(x>low&&x<=high)
+	   bin=i;
+	 
+       }
     
     return histo->GetBinCenter(bin);
     
-  }
+}
+
   
       // ----------member data ---------------------------
-      
-      edm::InputTag src_;        //input Collection
-      edm::FileInPath fileName_; //Name of the file with the histograms
-
-      //Smear MC particle!
-      bool smearMCParticle_;
-      //      bool smearConstituents_;
-
-      //Histogram Smearing
-      bool smearFromPtHistogram_;
-      bool smearFromEtaHistogram_;
-      std::string ptHisto_;        //Histogram name in File
+      edm::InputTag src_;           //input Collection
+      edm::FileInPath fileName_;    //Name of the file with the histograms
+      bool smearMCParticle_;        //Smear MC particle instead of reconstructed particle
+      bool smearFromPtHistogram_;   //Smear from an ET  resolution histogram
+      bool smearFromEtaHistogram_;  //Smear from an Eta resolution histogram 
+      std::string ptHisto_;         //Histogram name in File
       std::string etaHisto_;        //Histogram name in File
-
       //Flat smearing
-      double energyScale_;        //energy Scale 
+      double energyScale_;          //energy Scale 
+      double deltaEta_;             //delta Eta
+      double deltaPt_;              //delta Pt
 
-      //      double hadronEnergyScale_;        //hadronEnergy Scale 
-      //      double gammaEnergyScale_;        //Gamma Energy Scale 
-
-      double deltaEta_;           //delta Eta
-      double deltaPt_;            //delta Pt
-
+      //Random number generator services
+      edm::Service<edm::RandomNumberGenerator> rng;
+      CLHEP::RandFlat* flatDistribution; 
+      CLHEP::RandGauss* gaussPt;  
+      CLHEP::RandGauss* gaussEta; 
 
       //Gaussian Smearing
       double gSigmaPt_;
@@ -281,12 +275,9 @@ class SmearedParticleProducer : public edm::EDProducer {
 
       //Private data
       TFile *f;
-
       TH1F *ptHisto;
       TH1F *etaHisto;
-
-      edm::Service<edm::RandomNumberGenerator> rng;
 };
 
 
-
+#endif
--- plugins/SmearedTauProducer.h
+++ plugins/SmearedTauProducer.h
@@ -13,59 +13,44 @@ class SmearedTauProducer : public SmearedParticleProducer<pat::Tau,GenJetRetriev
 
     }
 
-
-
   ~SmearedTauProducer() {}
     
    protected:
   virtual void beginJob(const edm::EventSetup& es) {}
   virtual void endJob() {}
-
+  
   virtual void produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
     using namespace edm;
     using namespace reco;
 
     std::auto_ptr<std::vector<pat::Tau> > out(new std::vector<pat::Tau> );
     Handle<std::vector<pat::Tau> > srcH;
-  
     if(iEvent.getByLabel(src_,srcH) &&srcH->size()>0) 
       for(unsigned int i=0;i<srcH->size();++i) {
-
 	pat::Tau  object = srcH->at(i);
 	smear(object);
 
         if(smearConstituents_) {
-	
 	  math::XYZTLorentzVector hadronLV;
 	  PFCandidateRefVector hadrons = object.signalPFChargedHadrCands();
+
 	  if(hadrons.size()>0)
 	    for(unsigned int i=0;i<hadrons.size();++i)
-	    hadronLV+=hadrons.at(i)->p4();
+	      hadronLV+=hadrons.at(i)->p4();
 	  //apply hadron energy scale
-	  
-	  math::PtEtaPhiMLorentzVector vechES(hadronLV.pt()*hadronEnergyScale_,hadronLV.eta(),hadronLV.phi(),hadronLV.M());
-	  math::XYZTLorentzVector cartVechES(vechES.px(),vechES.py(),vechES.pz(),vechES.energy());
-	  hadronLV=cartVechES;
-	  
+	  hadronLV=hadronEnergyScale_*hadronLV;
 	  math::XYZTLorentzVector gammaLV;
 	  PFCandidateRefVector gammas = object.signalPFGammaCands();
+
 	  if(gammas.size()>0)
 	    for(unsigned int i=0;i<gammas.size();++i)
 	      gammaLV+=gammas.at(i)->p4();
-	  
-	
-	  math::PtEtaPhiMLorentzVector vecgES(gammaLV.pt()*gammaEnergyScale_,gammaLV.eta(),gammaLV.phi(),gammaLV.M());
-	  math::XYZTLorentzVector cartVecgES(vecgES.px(),vecgES.py(),vecgES.pz(),vecgES.energy());
-	  gammaLV=cartVecgES;
-	  
-	  object.setP4(gammaLV+hadronLV);
+	  gammaLV=gammaEnergyScale_*gammaLV;
+ 	  object.setP4(gammaLV+hadronLV);
 	}
-	
 	out->push_back(object);
       }
-
     iEvent.put(out);
-
   } 
 
   
