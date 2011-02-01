007398e5c1c0b13eaf81aaf2086efb524a0f7b9a
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2011-02-01 11:49:43 +0000

    generalized muon id. selection (replacing PATMuonVbTfSelector by PATMuonIdSelector class  and updating patMuonSelection_cfi.py and python/patLeptonSelection_cff.py accordingly)

--- plugins/PATMuonIdSelector.h
+++ plugins/PATMuonIdSelector.h
@@ -1,15 +1,16 @@
 
-/** \class PATMuonVbTfSelector
+/** \class PATMuonIdSelector
  *
  * Selection of "good quality" muons according to criteria
- * defined by Vector Boson Task Force and documented in Analysis Note CMS AN-10-264
+ * defined by Vector Boson Task Force and documented in Analysis Note CMS AN-10/264
+ * (including extension to WW cross-section analysis documented in CMS AN-10/344)
  *
  * \author Michail Bachtis,
  *         Christian Veelken
  *
- * \version $Revision: 1.1 $
+ * \version $Revision: 1.2 $
  *
- * $Id: PATMuonVbTfSelector.h,v 1.1 2010/09/26 16:55:30 veelken Exp $
+ * $Id: PATMuonIdSelector.h,v 1.2 2010/09/28 11:23:36 jkolb Exp $
  *
  */
 
@@ -22,13 +23,13 @@
 
 #include <vector>
 
-class PATMuonVbTfSelectorImp
+class PATMuonIdSelectorImp
 {
  public:
   typedef pat::MuonCollection collection;
 
-  explicit PATMuonVbTfSelectorImp(const edm::ParameterSet&);
-  ~PATMuonVbTfSelectorImp();
+  explicit PATMuonIdSelectorImp(const edm::ParameterSet&);
+  ~PATMuonIdSelectorImp();
 
   std::vector<const pat::Muon*>::const_iterator begin() const { return selected_.begin(); }
   std::vector<const pat::Muon*>::const_iterator end()   const { return selected_.end();   }
@@ -38,13 +39,23 @@ class PATMuonVbTfSelectorImp
   size_t size() const { return selected_.size(); }
 
  private:
+  void print();
+
   std::vector<const pat::Muon*> selected_;
 
 //--- configuration parameters
   edm::InputTag srcBeamSpot_;
+  edm::InputTag srcVertex_;
 
-  double   maxIPxy_;            // max. transverse impact parameter of muon (global) track wrt. beam spot
+  bool     applyGlobalMuonPromptTight_;
+  bool     applyAllArbitrated_;
+
+  double   maxIPxy_;            // max. transverse   impact parameter of muon track
+  double   maxIPz_;             // max. longitudinal impact parameter of muon track
+  int      IPtrackType_;        // compute impact parameters for inner/global track of muon
+  int      IPrefType_;          // compute impact parameters wrt. beam spot/reconstructed event vertex
   double   maxChi2red_;         // max. (normalized) chi^2 of global muon track fit per degree of freedom
+  double   maxDptOverPt_;       // max. relative error on muon momentum (computed for inner track)
   unsigned minTrackerHits_;     // min. number of hits in SiStrip + Pixel detectors
   unsigned minPixelHits_;       // min. number of hits in Pixel detector
   unsigned minMuonChamberHits_; // min. number of hits in Muon chambers
