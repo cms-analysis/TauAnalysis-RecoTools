74a7d03cf1a35b42e8c483d85ceda5d468f88d37
cmsbuild <>
2011-06-13 10:04:38 +0000

    This commit was manufactured by cvs2git to create tag 'TauAnalysis- RecoTools-b4_2_x_2011Jun17'.
    Sprout from master 2011-06-13 09:37:44 UTC Jeff Kolb <sha1-db54915842bc6be6c5255beebf3371b954cb71fa@cern.ch> 'first commit'
    Cherrypick from master 2011-03-29 17:51:32 UTC Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch> 'CMSSW_4_2_x compatible versions of config files, adapted to migration':
        python/prePatProduction_cff.py
    Cherrypick from TauAnalysis-RecoTools-B4_2_X 2011-06-13 10:04:36 UTC Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch> 'update to CMSSW_4_2_x':
        python/recoMuonIsolation_cfi.py

--- python/prePatProduction_cff.py
+++ python/prePatProduction_cff.py
@@ -5,28 +5,16 @@ import FWCore.ParameterSet.Config as cms
 #--------------------------------------------------------------------------------
 
 # produce particle flow based muon isolation quantities
-# for CMSSW_4_2_0_pre8 and higher
-#from CommonTools.ParticleFlow.pfNoPileUp_cff import *
-#from CommonTools.ParticleFlow.ParticleSelectors.pfAllChargedHadrons_cfi import *
-#from CommonTools.ParticleFlow.ParticleSelectors.pfAllNeutralHadrons_cfi import *
-#from CommonTools.ParticleFlow.ParticleSelectors.pfAllPhotons_cfi import *
-# for CMSSW_3_8_x and CMSSW_4_1_x release series
-from PhysicsTools.PFCandProducer.pfNoPileUp_cff import *
-from PhysicsTools.PFCandProducer.ParticleSelectors.pfAllChargedHadrons_cfi import *
-from PhysicsTools.PFCandProducer.ParticleSelectors.pfAllNeutralHadrons_cfi import *
-from PhysicsTools.PFCandProducer.ParticleSelectors.pfAllPhotons_cfi import *
-
+from CommonTools.ParticleFlow.pfNoPileUp_cff import *
+from CommonTools.ParticleFlow.ParticleSelectors.pfAllChargedHadrons_cfi import *
+from CommonTools.ParticleFlow.ParticleSelectors.pfAllNeutralHadrons_cfi import *
+from CommonTools.ParticleFlow.ParticleSelectors.pfAllPhotons_cfi import *
 from TauAnalysis.RecoTools.recoElectronIsolation_cfi import *
 from TauAnalysis.RecoTools.recoMuonIsolation_cfi import *
 
 # compute electron and tau id. quantities specific to EWK-tau analyses
 from TauAnalysis.RecoTools.recoElectronIdentification_cfi import *
 from TauAnalysis.RecoTools.recoPFTauIdentification_cfi import *
-
-# produce collections of kT PFJets for dR = 0.6 needed for rho (FastJet) pile-up corrections
-from RecoJets.Configuration.RecoPFJets_cff import *
-kt6PFJets.Rho_EtaMax = cms.double(2.5)
-kt6PFJets.doRhoFastjet = True
 
 # produce collections of dR = 0.07 and dR = 0.15 fixed
 # and dR = 5.0/Et shrinking signal cone taus using latest tags;
@@ -53,7 +41,6 @@ producePrePat = cms.Sequence(
    + pfAllChargedHadrons + pfAllNeutralHadrons + pfAllPhotons
    + recoElectronIsolation
    + recoMuonIsolation
-   + kt6PFJets 
    + PFTau
    # CV: discriminator against calo. muons currently disabled per default;
    #     add manually
